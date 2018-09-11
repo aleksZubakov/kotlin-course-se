8a47968a9cef8eed9963e4ccd1392a8379f318be
Zubakov <aleks.zubakov@gmail.com>
2018-09-11 22:53:32 +0300

    build.gradle change, task solution

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,56 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
 
-    return words.joinToString(separator = " ")
+class FunComparator(private val array: IntArray) : Comparator<Int> {
+    override fun compare(o1: Int, o2: Int): Int {
+        return array[o2] - array[o1]
+    }
+}
+
+data class Data<T, D>(
+        val array: T,
+        val priorityQueue: PriorityQueue<D>,
+        val n: Int,
+        val k: Int
+)
+
+fun readData(): Data<IntArray, Int> = Scanner(System.`in`).use {
+    val n = it.nextInt()
+    val k = it.nextInt()
+
+    val array = IntArray(n)
+    val priorityQueue = PriorityQueue(FunComparator(array))
+
+    for (i in 0 until n) {
+        array[i] = it.nextInt()
+        if (i < k) {
+            priorityQueue.add(i)
+        }
+    }
+
+    return Data(array, priorityQueue, n, k)
+}
+
+fun solve() {
+    val (array, pq, n, k) = readData()
+
+    var ans: Long = 0
+    val seq = IntArray(n)
+    for (i in 0 until n) {
+        if (i + k < n)
+            pq.add(i + k)
+        val ind = pq.poll()
+        ans += 1L * (i + k - ind).toLong() * array[ind].toLong()
+        seq[ind] = i + 1 + k
+
+    }
+
+    println(ans)
+    println(seq.joinToString(" "))
+
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    solve()
 }
